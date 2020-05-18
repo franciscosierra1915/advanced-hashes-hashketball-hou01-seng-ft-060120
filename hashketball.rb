19e72a142be15da5ce295792498d6529e6c1d70c
Francisco Sierra <fransierra.s.15@gmail.com>
2020-05-18 21:17:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -163,7 +163,7 @@ def player_numbers(team_name)
   jersey = []
  game_hash.each do |location, team_data|
    if team_data[:team_name] == team_name
-     jersey << team_data[:players][:number]
+     jersey << team_data[:players][:number].to_i
    end
  end
     jersey
